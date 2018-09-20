3925ab92e0e55519837906d363c9d00c2dcb131e
Morgan <mgomez46@gatech.edu>
2018-09-20 15:57:01 -0400

    Initial commit

---
+++ app/src/main/java/com/length6array/donationtracker2/Welcome.java
@@ -0,0 +1,13 @@
+package com.length6array.donationtracker2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
