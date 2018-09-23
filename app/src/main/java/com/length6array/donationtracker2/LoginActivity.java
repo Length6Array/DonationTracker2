8a28f8500a2ed44df8b18f1fcc30fe3ad4304a8d
Morgan <mgomez46@gatech.edu>
2018-09-23 12:22:05 -0400

    Messing with username and password stuff

--- app/src/main/java/com/length6array/donationtracker2/LoginActivity.java
+++ app/src/main/java/com/length6array/donationtracker2/LoginActivity.java
@@ -5,9 +5,6 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -34,6 +31,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
@@ -44,17 +42,10 @@ import static android.Manifest.permission.READ_CONTACTS;
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
     /**
-     * Id to identity READ_CONTACTS permission request.
+     * Stores Email and passwords
      */
-    private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
+    protected static HashMap<String, String> credentials = new HashMap<>();
+    protected static ArrayList<String> emails = new ArrayList<>();
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -72,7 +63,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         setContentView(R.layout.activity_login);
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -108,50 +98,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
-
-    private void populateAutoComplete() {
-        if (!mayRequestContacts()) {
-            return;
-        }
-
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-    private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-        }
-        return false;
-    }
-
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
-            }
-        }
-    }
-
 
     /**
      * Attempts to sign in or register the account specified by the login form.
@@ -319,7 +265,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
 
             try {
                 // Simulate network access.
@@ -328,25 +273,21 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
 
-            boolean found = false;
-            for (String credential : CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    found = true;
-                    return pieces[1].equals(mPassword);
-
+            if (emails.contains(mEmail)) {
+                if (credentials.get(mEmail).equals(mPassword)) {
+                    return true;
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Password incorrect. Please try again.",
+                            Toast.LENGTH_SHORT);
+                    return false;
                 }
+            } else{
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Email not found. Please register.",
+                        Toast.LENGTH_SHORT);
+                return false;
             }
-            if (!found){
-                CharSequence notFound = "Email not registered. Please go to registration";
-//                int duration = Toast.LENGTH_SHORT;
-//                Context context = getApplicationContext();
-//                Toast noFound = Toast.makeText(context, notFound, duration);
-//                noFound.show();
-            }
-
-            return true;
         }
 
         @Override
@@ -368,5 +309,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
         }
     }
+
+    public static ArrayList<String> getEmails(){
+        return emails;
+    }
+    public static void setEmails(String email, String password){
+        emails.add(email);
+        credentials.put(email, password);
+    }
+
 }
 
--- app/src/main/java/com/length6array/donationtracker2/Registration.java
+++ app/src/main/java/com/length6array/donationtracker2/Registration.java
@@ -3,10 +3,6 @@ package com.length6array.donationtracker2;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -16,6 +12,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 
+import android.util.Log;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -31,6 +28,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import android.widget.Toast;
 import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
@@ -47,11 +45,12 @@ public class Registration extends AppCompatActivity implements LoaderCallbacks<C
 
     /**
      * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
+
+
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -136,11 +135,6 @@ public class Registration extends AppCompatActivity implements LoaderCallbacks<C
             focusView = mPasswordView;
             cancel = true;
         }
-//        if (!(password.equals(retypedPassword))){
-//            reTypePassword.setError("Passwords do not match");
-//            focusView = reTypePassword;
-//            cancel = true;
-//        }
 
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
@@ -280,25 +274,28 @@ public class Registration extends AppCompatActivity implements LoaderCallbacks<C
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
             try {
                 // Simulate network access.
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 return false;
             }
+           // LoginActivity login = new LoginActivity();
+            //ArrayList<String> emails = LoginActivity.getEmails();
+            if (LoginActivity.getEmails().contains(mEmail)){
+                Log.i("Registration", "Found taken email");
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Email already taken. Log on or use new email.",
+                        Toast.LENGTH_SHORT);
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
+                toast.show();
+                return false;
+            } else {
+                Log.i("Registration", "Making new account");
+               // if (reTypePassword.equals())
+                LoginActivity.setEmails(mEmail, mPassword);
+                return true;
             }
-
-            // TODO: register the new account here.
-            return true;
         }
 
         @Override
