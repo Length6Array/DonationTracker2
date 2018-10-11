c8cce5e6d345412b18966c98aa0d65c63049f68b
Morgan <mgomez46@gatech.edu>
2018-10-10 20:27:54 -0400

    + location.java, allowed for parsing of .csv file

---
+++ app/src/main/java/com/length6array/donationtracker2/Location.java
@@ -0,0 +1,104 @@
+package com.length6array.donationtracker2;
+
+public class Location {
+
+    private int key;
+    private String name;
+    private float latitude;
+    private float longitude;
+    private String address;
+    private String city;
+    private String state;
+    private int zipCode;
+    private String type;
+    private String phone;
+    private String website;
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+}
--- app/src/main/java/com/length6array/donationtracker2/MainActivity.java
+++ app/src/main/java/com/length6array/donationtracker2/MainActivity.java
@@ -3,8 +3,17 @@ package com.length6array.donationtracker2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +29,48 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Welcome.class));
             }
         });
+        readLocations();
     }
+
+    private List<Location> locations = new ArrayList<Location>();
+    private void readLocations(){
+        InputStream is = getResources().openRawResource(R.raw.locations);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8") )
+        );
+
+        String line = "";
+        try {
+            //this steps over the header!
+            reader.readLine();
+
+            while ( (line = reader.readLine()) != null){
+                //this splits the data
+                String[] tokens = line.split(",");
+
+                //actually parsing and setting
+                //TODO error checking
+                Location newLocation = new Location();
+                newLocation.setKey(Integer.parseInt(tokens[0]));
+                newLocation.setName(tokens[1]);
+                newLocation.setLatitude(Float.parseFloat(tokens[2]));
+                newLocation.setLongitude(Float.parseFloat(tokens[3]));
+                newLocation.setAddress(tokens[4]);
+                newLocation.setCity(tokens[5]);
+                newLocation.setState(tokens[6]);
+                newLocation.setZipCode(Integer.parseInt(tokens[7]));
+                newLocation.setType(tokens[8]);
+                newLocation.setPhone(tokens[9]);
+                newLocation.setWebsite(tokens[10]);
+
+                locations.add(newLocation);
+
+                Log.i("MainActivity", "Just Added" + newLocation);
+            }
+        }catch (IOException e){
+            Log.i("MainActivity", "Error reading Location Data");
+            e.printStackTrace();
+        }
+    }
+
 }
